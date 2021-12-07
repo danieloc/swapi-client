985dd2bc0996a06552c56948b6b8efbc38b5d53c
Daniel O'Connor <daniel.o.c.03@gmail.com>
2021-12-07 18:32:23 +0000

    Add provisional api

---
+++ src/api/users.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+type UserResponse = {
+  count: number;
+  results: User[];
+};
+
+type User = {
+  name: string;
+  height: string;
+  mass: number;
+  created: Date;
+  edited: Date;
+  planetName: string;
+};
+
+ export const getUsers = async (page: number) => {
+    try {
+      await axios.get<UserResponse>(`http://swapi.dev/api/people?page=${page}`);
+    } catch (e) {
+        
+    }
+  },
+};
