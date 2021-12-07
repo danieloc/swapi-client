d0dcc0070533a79e9dba9e1817b836dae1078df3
Daniel O'Connor <daniel.o.c.03@gmail.com>
2021-12-07 22:07:49 +0000

    Add users to homepage instead of hello world

---
+++ src/components/Users.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>abc</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Users",
+});
+</script>
+
+<style scoped lang="scss">
+//Styles
+</style>
