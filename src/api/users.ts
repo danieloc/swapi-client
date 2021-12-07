d5462e56fd54bb46668742ee5af8be6ff1d72bb3
Daniel O'Connor <daniel.o.c.03@gmail.com>
2021-12-07 19:41:04 +0000

    Log if there was an error

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,18 +1,26 @@
 import axios from "axios";
 import { User } from "../types";
 
-type UserResponse = {
-  count: number;
-  results: User[];
+type ApiResponse = {
+  totalCount: number;
+  users: User[];
 };
 
-export const getUsers = async (page: number) => {
+export const getUsers = async (
+  page: number
+): Promise<ApiResponse | "ERROR"> => {
   try {
-    const { data } = await axios.get<UserResponse>(
-      `http://swapi.dev/api/people?page=${page}`
-    );
+    const { data } = await axios.get<{
+      count: number;
+      results: User[];
+    }>(`http://swapi.dev/api/people?page=${page}`);
     return { totalCount: data.count, users: data.results };
   } catch (e) {
     // TODO: Add an error state for failing to fetch people
+    console.log(
+      "There was an error while trying to fetch the users from swapi.",
+      e
+    );
+    return "ERROR";
   }
 };
