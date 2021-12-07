e3263df6681aa042ab135a94e2ecb0550b9d27ac
Daniel O'Connor <daniel.o.c.03@gmail.com>
2021-12-07 19:24:42 +0000

    Adjust types on store, make more typesafe

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,44 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { GetterTree, MutationTree, ActionTree } from "vuex";
+import { User } from "@/types";
+import { getUsers } from "@/api/users";
 
 Vue.use(Vuex);
 
+type State = {
+  users: User[];
+};
+
+type RootState = State;
+
+const initState: State = {
+  users: [],
+};
+
+const getters: GetterTree<State, RootState> = {
+  getUsers: (state) => {
+    return state.users;
+  },
+};
+
+const mutations: MutationTree<State> = {
+  setUsers(state, payload) {
+    state.users = payload;
+  },
+};
+
+const actions: ActionTree<State, RootState> = {
+  fetchUserId: async () => {
+    const response = await getUsers(1);
+    if (response) {
+    }
+  },
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: initState,
+  mutations: mutations,
+  actions: actions,
+  getters,
 });
