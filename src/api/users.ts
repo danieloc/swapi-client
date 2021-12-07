373fc5c4a2ef4eb937d989d0e905216ffce34844
Daniel O'Connor <daniel.o.c.03@gmail.com>
2021-12-07 19:24:20 +0000

    Fix typeing for request

--- src/api/users.ts
+++ src/api/users.ts
@@ -8,7 +8,10 @@ type UserResponse = {
 
 export const getUsers = async (page: number) => {
   try {
-    await axios.get<UserResponse>(`http://swapi.dev/api/people?page=${page}`);
+    const { data } = await axios.get<UserResponse>(
+      `http://swapi.dev/api/people?page=${page}`
+    );
+    return { totalCountUsers: data.count, users: data.results };
   } catch (e) {
     // TODO: Add an error state for failing to fetch people
   }
